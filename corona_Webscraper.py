9b27ac271be52763a2795c529292981f733ef237
Subinsubedi <45024516+Subinsubedi@users.noreply.github.com>
2020-07-26 16:59:14 -0400

    Update corona_Webscraper.py

--- corona_Webscraper.py
+++ corona_Webscraper.py
@@ -1,27 +1,5 @@
 
-'''
-import requests
-from bs4 import BeautifulSoup
 
-jblCharge4URL = 'https://www.amazon.de/JBL-Charge-Bluetooth-Lautsprecher-Schwarz-      integrierter/dp/B07HGHRYCY/ref=sr_1_2_sspa?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&  keywords=jbl+charge+4&qid=1562775856&s=gateway&sr=8-2-spons&psc=1'
-
-def get_page(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    return soup
-
-def get_product_name(url):
-    soup = get_page(url)
-    try:
-        title = soup.find(id="productTitle")
-        print("SUCCESS")
-
-    except AttributeError:
-        print("ERROR")
-    return(title)
-       
-print(get_product_name(jblCharge4URL))
-'''
 import requests
 from bs4 import BeautifulSoup
 URL = "https://www.worldometers.info/coronavirus/"
@@ -38,5 +16,4 @@ print(results[1].text)
 print(results[2].text)
 
 
-#hmm
 #print(results.prettify())
